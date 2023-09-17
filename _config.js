e3c97f28a9d28469d382c5068d50c7f3f50a2c54
The-Algorist <waruithemystery@gmail.com>
2023-09-17 15:58:49 +0300

    Replaced mongoURI with the updated URI in the config.js file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://gedionwarui:123456Moringa@ip-cluster.89p06wu.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://gedionwarui:123456Moringa@ip-cluster.89p06wu.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://gedionwarui:123456Moringa@ip-cluster.89p06wu.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
